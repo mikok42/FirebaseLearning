6d4a0164b5a5aadd23b7c9197a0f0b651908ed8c
mikok42 <mikolaj@linczewski.com>
2021-05-24 17:07:35 +0200

    firebase

---
+++ FirebaseTutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseTutorial
+//
+//  Created by Mikołaj Linczewski on 24/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
